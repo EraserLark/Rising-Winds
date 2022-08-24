eed69efdf56da3397bb13256d5e248c51f0e55df
eraserlark <eraserlark@gmail.com>
2022-08-24 11:25:23 -0400

    Add basic dialogue functions and dictionary

--- NovelInterface.gd
+++ NovelInterface.gd
@@ -2,17 +2,36 @@ extends Control
 
 onready var choiceButtons = $CenterContainer/VBoxContainer
 onready var textBox = $Panel/RichTextLabel
+onready var nameLabel = $Panel/NameLabel
+
+#Dialogue Dictionary Test
+var masterDict = {1: {"Name": "Tony", "Dialogue": "Howdy howdy."},
+2: {"Name": "Bismelda", "Dialogue": "Who are you?"}}
+var dictCount : int = 1
+var masterDictSize = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	choiceButtons.hide()
+	
+	masterDictSize = masterDict.size()
+	displayMessage(masterDict[dictCount])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		if dictCount < masterDictSize:
+			dictCount += 1
+			displayMessage(masterDict[dictCount])
+		else:
+			textBox.hide()
 
-func _on_Button_pressed():
-	changeText("Button Pressed!")
+func displayMessage(dict):
+	nameLabel.text = dict["Name"]
+	changeText(dict["Dialogue"])
 
 func changeText(newText):
 	textBox.text = newText
+	
+func _on_Button_pressed():
+	changeText("Button Pressed!")
