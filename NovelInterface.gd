a5ec6dc695e156a9c8f3216ad783c6456767e418
eraserlark <eraserlark@gmail.com>
2022-08-27 16:48:15 -0400

    Add Choice Structure, Reorganize Animation Nodes

--- NovelInterface.gd
+++ NovelInterface.gd
@@ -4,9 +4,32 @@ onready var choiceButtons = $CenterContainer/VBoxContainer
 onready var textPanel = $Panel
 onready var textBox = $Panel/RichTextLabel
 onready var nameLabel = $Panel/NameLabel
+signal choice_selected(answer)
 
 func _ready():
-	pass # Replace with function body.
+	pass
+
+#Creates new buttons corresponding to the choice data sent in
+func newChoice(choices):
+	clearChoices()
+	for i in choices:
+		var button = Button.new()
+		var choiceText = choices[i]["Text"]
+		var choiceRoute = choices[i]["Route"]
+		button.text = choiceText
+		button.connect("pressed", self, "_button_pressed", [choiceRoute])
+		choiceButtons.add_child(button)
+
+func _button_pressed(chosenRoute):
+	textboxReveal(true)
+	buttonReveal(false)
+	emit_signal("choice_selected", chosenRoute)
+
+#Removes all old buttons from last choice
+func clearChoices():
+	for n in choiceButtons.get_children():
+		choiceButtons.remove_child(n)
+		n.queue_free()
 
 func changeName(name):
 	nameLabel.text = name
--- NovelStage.gd
+++ NovelStage.gd
@@ -5,6 +5,7 @@ onready var novInterface = $NovelInterface
 onready var actor0 = $Actor0
 onready var actor1 = $Actor1
 onready var actor2 = $Actor2
+onready var animPlayer = $AnimationPlayer
 var actorArray
 
 #Dialogue Dictionary Test
@@ -13,37 +14,64 @@ onready var masterDict = {
 2: {"Actor": 1, "Name": actor1.actorName, 	"Face": "Angry", 	"Dialogue": "Who are you?", "Anim": null},
 3: {"Actor": 0, "Name": actor0.actorName,	"Face": "Normal", 	"Dialogue": "Sorry to bother you, name's " + actor0.actorName, "Anim": null},
 4: {"Actor": 1, "Name": actor1.actorName, 	"Face": "Sad", 		"Dialogue": "No worries. Been a long day.", "Anim": null },
-5: {"Actor": 2, "Name": actor2.actorName, 	"Face": "Happy", 	"Dialogue": "Hee hee! I'm here too!", "Anim": "MoveIn"}
+5: {"Actor": 2, "Name": actor2.actorName, 	"Face": "Happy", 	"Dialogue": "Hee hee! I'm here too!", "Anim": "testPeekDown"}
 }
 var dictCount : int = 1
-var masterDictSize = 0
+var route
+var routeSize = 0
+
+onready var jimmyDict = {
+1: {"Actor": 2, "Name": actor2.actorName, 	"Face": "Happy", 	"Dialogue": "That's my name! Don't wear it out!", "Anim": null},
+2: {"Actor": 1, "Name": actor1.actorName, 	"Face": "Angry", 	"Dialogue": "Get off the ceiling!", "Anim": null},
+3: {"Actor": 0, "Name": actor0.actorName,	"Face": "Normal", 	"Dialogue": "Now, now " + actor1.actorName + ". It's okay to have fun.", "Anim": null}
+}
+
+#Choice Dictionary Test
+onready var choiceDict = {
+0: {"Text": actor0.actorName, "Route": masterDict},
+1: {"Text": actor1.actorName, "Route": masterDict},
+2: {"Text": actor2.actorName, "Route": jimmyDict}
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	novInterface.connect("choice_selected", self, "nextChoice")
+	
+	route = masterDict
+	routeStart(route)
+
+func routeStart(_route):
 	novInterface.buttonReveal(false)	#Hide buttons
 	
 	actorArray = [actor0, actor1, actor2]
 	
-	masterDictSize = masterDict.size()
-	nextLine(masterDict[dictCount])
+	routeSize = _route.size()
+	nextLine(_route[dictCount])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
-		if dictCount < masterDictSize:
+		if dictCount < routeSize:
 			dictCount += 1
-			nextLine(masterDict[dictCount])
+			nextLine(route[dictCount])
 		else:
 			novInterface.textboxReveal(false)
 			novInterface.buttonReveal(true)
+			novInterface.newChoice(choiceDict)
 
 func nextLine(dict):
 	var actor = actorArray[dict["Actor"]]
 	actor.changeFace(dict["Face"])
 	if dict["Anim"] != null:
-		actor.playAnimation(dict["Anim"])
+		#actor.playAnimation(dict["Anim"])
+		animPlayer.play(dict["Anim"])
 	novInterface.changeName(dict["Name"])
 	novInterface.changeText(dict["Dialogue"])
+
+func nextChoice(nextRoute):
+	route = nextRoute
+	dictCount = 1
+	routeStart(nextRoute)
 
 #Signal disconnected, moved script
 #func _on_Button_pressed():
