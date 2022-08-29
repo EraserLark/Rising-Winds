f79861c40f6996e53fec85fc3fedcbc85675a743
eraserlark <eraserlark@gmail.com>
2022-08-29 17:13:21 -0400

    Refine updateCast() func, clean up code

--- NovelStage.gd
+++ NovelStage.gd
@@ -2,14 +2,10 @@ extends Node
 
 onready var novInterface = $NovelInterface
 onready var cast = $Cast
-
-#Add createActor() func
-var actorTemp = preload("res://Actors/Actor.tscn")
-onready var actor0 = $Cast/Actor0
-onready var actor1 = $Cast/Actor1
-onready var actor2 = $Cast/Actor2
 onready var animPlayer = $AnimationPlayer
-var actorArray
+
+var actorTemp = preload("res://Actors/Actor.tscn")
+var actorArray : Array
 
 export (String) var jsonDialogue
 var convos
@@ -18,17 +14,12 @@ var dictCount : int = 0
 var route
 var routeSize = 0
 
-var actorDict = {
-	1: {"Name": "Actor0", "Position": Vector2(152, 376), "Info": "res://TestAssets/TestActor.tres"}
-}
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	convos = loadJSONFile(jsonDialogue)
 	novInterface.connect("choice_selected", self, "nextChoice")
-	updateCast(actorDict)
-	
-	route = routeStart(convos, "masterDict")
+	updateCast(convos, "actorDict")
+	routeStart(convos, "masterDict")
 
 #https://www.youtube.com/watch?v=8HOmLNuuccs&t=178s
 func loadJSONFile(file_path):
@@ -40,22 +31,25 @@ func loadJSONFile(file_path):
 	assert(jsonFile.size() > 0)
 	return jsonFile
 
-func updateCast(castDict):
-	for obj in castDict:
+func updateCast(jsonObj, castDict):
+	var newCast = jsonObj[castDict]
+	for obj in newCast:
 		var newActor = actorTemp.instance()
-		newActor.name = actorDict[obj]["Name"]
-		newActor.actorInfo = load(actorDict[obj]["Info"])
-		newActor.position = actorDict[obj]["Position"]
+		newActor.name = obj["Name"]
+		newActor.actorInfo = load(obj["Info"])
+		newActor.position = (str2var(obj["Position"]))	#Vector2
 		cast.add_child(newActor)
+	
+	actorArray = cast.get_children()
 
 func routeStart(dialogueObj, routeName):
+	setRoute(dialogueObj, routeName)	
 	novInterface.buttonReveal(false)	#Hide buttons
-	actorArray = [actor0, actor1, actor2]
-	
-	var _route = dialogueObj[routeName]
-	routeSize = _route.size()
-	nextLine(_route[dictCount])
-	return _route
+	nextLine(route[dictCount])
+
+func setRoute(dialogObj, routeName):
+	route = dialogObj[routeName]
+	routeSize = route.size()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -64,7 +58,7 @@ func _process(delta):
 		if dictCount < routeSize:
 			nextLine(route[dictCount])
 		else:
-			showChoices()
+			showChoices(convos["choiceDict"])
 
 func nextLine(dict):
 	var actor = actorArray[dict["Actor"]]
@@ -75,8 +69,8 @@ func nextLine(dict):
 	novInterface.changeName(dict["Name"])
 	novInterface.changeText(dict["Dialogue"])
 
-func showChoices():
-	novInterface.newChoice(convos["choiceDict"], false)
+func showChoices(choicesDict):
+	novInterface.newChoice(choicesDict, false)
 
 func nextChoice(nextRoute):
 	#route = convos[nextRoute]
