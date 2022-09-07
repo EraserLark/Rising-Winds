3ce65c9eca2028037a1e5c0c3d898c6525d569ad
eraserlark <eraserlark@gmail.com>
2022-09-07 19:07:10 -0400

    Implement dialogue speed tags

--- Novel/NovelInterface.gd
+++ Novel/NovelInterface.gd
@@ -6,38 +6,59 @@ onready var textBox = $Panel/RichTextLabel
 onready var nameLabel = $Panel/NameLabel
 
 onready var typeTimer = $Panel/Timer
+var defaultTypeSpeed = 0.05
 var charIndex : int = 0
 var currentDialogue
+var inTag = false
+var skip = false
 signal phraseFin()
 
 #var typeSpeed = 30
 
 func _ready():
-	setTimerSpeed(0.05)	#Set type speed
+	pass
 
-func setTimerSpeed(newSpeed):
-	typeTimer.set_wait_time(newSpeed)
+func setTimerSpeed(multiplier):
+	typeTimer.set_wait_time(defaultTypeSpeed * multiplier)
 
 func typeText(newText : String):
 	
 	textBox.clear()
 	showTextbox(true)
+	setTimerSpeed(1) #Set type speed
 	
 	currentDialogue = newText
 	charIndex = 0
+	skip = false
 	
+	#for loop instead?
 	while charIndex < currentDialogue.length():
-		typeTimer.start()
+		#https://youtu.be/jhwfA-QF54M?t=414
+		checkTag(currentDialogue, charIndex)
+		
+		if !inTag:
+			textBox.text += currentDialogue[charIndex]
+			if !skip:
+				typeTimer.start()
+				yield(typeTimer, "timeout")
+		
 		charIndex += 1
-		textBox.text = currentDialogue.substr(0, charIndex)
-		yield(typeTimer, "timeout")
 	
 	emit_signal("phraseFin")
 
+func checkTag(fullText, characterIndex):
+		if fullText[characterIndex] == "<":
+			var customWaitMult = int(fullText[characterIndex + 1])
+			setTimerSpeed(customWaitMult)
+			inTag = true
+		elif inTag:
+			if fullText[characterIndex - 1] == ">":
+				inTag = false
+
 func dialogueInput(event):
 	if event.is_action_pressed("ui_accept"):
-		textBox.text = currentDialogue
-		charIndex = currentDialogue.length()
+		typeTimer.emit_signal("timeout")	#Skips to end of current 'yield' timer, based off typing speed
+		skip = true
 
 func changeText(newText):
 	textBox.text = newText
