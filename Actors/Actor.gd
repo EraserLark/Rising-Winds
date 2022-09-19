07a13cd68371bbd908590c84bc4214062ff54ccf
eraserlark <eraserlark@gmail.com>
2022-09-19 17:51:03 -0400

    Reconfigure flipping actors

--- Actors/Actor.gd
+++ Actors/Actor.gd
@@ -1,3 +1,4 @@
+tool
 extends Node2D
 
 onready var facePt = $BodyPoint/FacePoint
@@ -5,9 +6,11 @@ onready var bodyPt = $BodyPoint
 onready var faceSpr = $BodyPoint/FacePoint/FaceSprite
 onready var bodySpr = $BodyPoint/BodySprite
 onready var animPlayer = $AnimationPlayer
+
 var actorName : String
 export(Resource) var actorInfo
 export(bool) var flipped setget change_flipped
+export(bool) var testBool
 export(String) var expFace setget changeFace
 export(String) var expPose setget changePose
 
@@ -19,20 +22,48 @@ func _ready():
 func changeFace(faceName):
 	var newFace : Texture = actorInfo.charFaces[faceName]
 	faceSpr.set_texture(newFace)
+	pass
 
 func changePose(poseName):
 	var newPose : Texture = actorInfo.charPoses[poseName]
 	bodySpr.set_texture(newPose)
-	#bodySpr.position = actorInfo.offsetData[poseName]
+	pass
 
 func playAnimation(anim):
 	animPlayer.play(anim)
 
 func change_flipped(case):
-#	var actorOrient = self.get_node("BodyPoint")
-	var actorOrient = self
-	var currentScale = actorOrient.scale
-	if case:
-		actorOrient.set_scale(Vector2(-currentScale.x, currentScale.y))
-	if !case:
-		actorOrient.set_scale(Vector2(currentScale.x, currentScale.y))
+	print(var2str(case) + self.name)
+	
+	bodySpr = get_node("BodyPoint/BodySprite")
+	faceSpr = get_node("BodyPoint/FacePoint/FaceSprite")
+	bodyPt = get_node("BodyPoint")
+	facePt = get_node("BodyPoint/FacePoint")
+	
+	bodySpr.flip_h = case
+	faceSpr.flip_h = case
+	
+	var flipNum = -1
+	if flipped == case:
+		flipNum = 1
+	
+	var currentPos
+	currentPos = facePt.position
+	facePt.position = Vector2((currentPos.x * flipNum), currentPos.y)
+	
+	currentPos = bodyPt.position
+	bodyPt.position = Vector2((currentPos.x * flipNum), currentPos.y)
+	
+	flipped = case
+	
+#	if bodySpr == null:
+	
+#	if case == true:
+
+#	elif case == false:
+#		var currentPos
+#		currentPos = facePt.position
+#		facePt.position = Vector2(abs(currentPos.x), currentPos.y)
+#
+#		currentPos = bodyPt.position
+#		bodyPt.position = Vector2(abs(currentPos.x), currentPos.y)
